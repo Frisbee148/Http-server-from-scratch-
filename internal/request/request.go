00a4ae6ae9cb0214ca108e4d8195e300d39d8246
Your Name <you@example.com>
2026-01-07 11:03:25 +0530

    updated request

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -11,6 +15,27 @@ type RequestLine struct {
 	RequestTarget string
 	Method        string
 }
+
+var ERROr_MALFORMED_REQUEST_LINE= fmt.Errorf("malformed requestline")
+var INCOMPLETE_START_LINE=fmt.Errorf("")
+var SEPARATOR="\r\n"
+
+func parseRequestLine(b []byte) (*RequestLine, string, error) {
+	idx := strings.Index(b, SEPARATOR)
+	if idx == -1{
+		return nil,b,nil
+
+	}
+	startLine:=b[:idx]
+	restOfMsg:=b[idx+len(SEPARATOR):]
+	parts :=strings.Split(startLine," ")
+	if len(parts)!=3{
+			return nil,restOfMsg,ERROr_MALFORMED_REQUEST_LINE
+		}
+	return &RequestLine{
+		Method: parts[0],
+		RequestTarget:parts[1],
+	},restOfMsg,nil
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
